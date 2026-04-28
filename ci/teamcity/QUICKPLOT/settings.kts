afd9beff89facb06cd09c9754746cf1130a121f6
hrajagers <hrajagers@gmail.com>
2026-04-28 14:39:44 +0200

    new syntax for checkoutDir ...

--- ci/teamcity/QUICKPLOT/settings.kts
+++ ci/teamcity/QUICKPLOT/settings.kts
@@ -428,7 +428,9 @@ object Linux_LnxRunQuickplotTestBenchStandalone : BuildType({
     buildNumberPattern = "${Windows_WinCompileQuickplot.depParamRefs.buildNumber}"
 
     vcs {
-        root(DslContext.settingsRoot, checkoutDir = "code")
+        root(DslContext.settingsRoot) {
+           checkoutDir = "code"
+        }
         root(AbsoluteId("Quickplot_DSCTestbenchTestsQuickplot"), "+:. = >testbench")
         root(AbsoluteId("Quickplot_ReposDsCommon"), "+:. => common")
 
