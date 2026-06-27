bcfcbb600faca3a2517961dd6df69b5932d8042f
hrajagers <hrajagers@gmail.com>
2026-06-27 15:07:53 +0200

    Correct number of hyphens for the separator

--- src/delft3d_matlab/private/flexmeshfil.m
+++ src/delft3d_matlab/private/flexmeshfil.m
@@ -225,7 +225,7 @@ else
 end
 if isfield(FI,'Bnd') && ~isempty(FI.Bnd)
     Out(end+1) = Out(end);
-    Out(end).Name = '------';
+    Out(end).Name = '-------';
     bndTypes = unique({FI.Bnd.Type});
     for b = 1:length(bndTypes)
         Out(end+1) = Out(end);
